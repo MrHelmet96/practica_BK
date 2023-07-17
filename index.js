7a22aa948be31418ed4b2764058d4c3925a71889
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-07-17 19:37:08 -0300

    instalando nodemon & mysql

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
 
+//conectarse a la BD
+var conection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'silicon'
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("base de datos conectada");
+    }
+})
+//fin conexión
+
+
+// código fuente
+
+
+//fin código
 app.listen(8080, (err) => {
     if (err) {
         console.log(err);
