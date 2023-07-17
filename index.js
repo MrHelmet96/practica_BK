fbc8aed27de71efd86c8758e6858b840b8a718d0
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-07-17 19:30:57 -0300

    practicando

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require('express');
-
 const app = express();
+
+app.listen(8080, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("servidor escuchando en el puerto 8080");
+    }
+});
